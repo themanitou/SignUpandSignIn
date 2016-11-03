d65e6d8112f021076d2073f0759e82999b503044
Quan Nguyen <quan.nguyen@dektech.com.au>
2016-11-03 17:27:10 +1100

    Initial commit

---
+++ app/src/main/java/com/hue/signupandsignin/app/AppConfig.java
@@ -0,0 +1,13 @@
+package com.hue.signupandsignin.app;
+
+/**
+ * Created by manitou on 11/3/16.
+ */
+
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://localhost:38080/android_login_api/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://localhost:38080/android_login_api/register.php";
+}
