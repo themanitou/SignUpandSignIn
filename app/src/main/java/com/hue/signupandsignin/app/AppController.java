9075edea81790f38d7a03b62f5332f73a6c353d9
Quan Nguyen <quan.nguyen@dektech.com.au>
2016-11-04 12:33:43 +1100

    Remove unused method from AppController

--- app/src/main/java/com/hue/signupandsignin/app/AppController.java
+++ app/src/main/java/com/hue/signupandsignin/app/AppController.java
@@ -40,11 +40,6 @@ public class AppController extends Application {
         getRequestQueue().add(req);
     }
 
-    public <T> void addToRequestQueue(Request<T> req) {
-        req.setTag(TAG);
-        getRequestQueue().add(req);
-    }
-
     public void cancelPendingRequests(Object tag) {
         if (mRequestQueue != null) {
             mRequestQueue.cancelAll(tag);
