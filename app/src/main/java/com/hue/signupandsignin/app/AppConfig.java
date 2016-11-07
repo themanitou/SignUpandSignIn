04fad27259301ed0624536595c576781fed433f3
Quan Nguyen <quan.nguyen@dektech.com.au>
2016-11-07 21:11:10 +1100

    Implement TrustManager to use self-signed server certificate

--- app/src/main/java/com/hue/signupandsignin/app/AppConfig.java
+++ app/src/main/java/com/hue/signupandsignin/app/AppConfig.java
@@ -6,8 +6,10 @@ package com.hue.signupandsignin.app;
 
 public class AppConfig {
     // Server user login url
-    public static String URL_LOGIN = "http://localhost:38080/android_login_api/login.php";
+    //public static String URL_LOGIN = "http://10.2.4.88:38080/android_login_api/login.php";
+    public static String URL_LOGIN = "https://192.168.1.101:38080/android_login_api/login.php";
 
     // Server user register url
-    public static String URL_REGISTER = "http://localhost:38080/android_login_api/register.php";
+    //public static String URL_REGISTER = "http://10.2.4.88:38080/android_login_api/register.php";
+    public static String URL_REGISTER = "https://192.168.1.101:38080/android_login_api/register.php";
 }
--- app/src/main/java/com/hue/signupandsignin/app/AppController.java
+++ app/src/main/java/com/hue/signupandsignin/app/AppController.java
@@ -5,10 +5,35 @@ package com.hue.signupandsignin.app;
  */
 import android.app.Application;
 import android.text.TextUtils;
+import android.util.Log;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.HurlStack;
 import com.android.volley.toolbox.Volley;
+import com.hue.signupandsignin.R;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.TrustManagerFactory;
+import javax.net.ssl.X509TrustManager;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
 
 public class AppController extends Application {
     public static final String TAG = AppController.class.getSimpleName();
@@ -29,7 +54,21 @@ public class AppController extends Application {
 
     public RequestQueue getRequestQueue() {
         if (mRequestQueue == null) {
-            mRequestQueue = Volley.newRequestQueue(getApplicationContext());
+            HurlStack hurlStack = new HurlStack() {
+                @Override
+                protected HttpURLConnection createConnection(URL url) throws IOException {
+                    HttpsURLConnection httpsURLConnection = (HttpsURLConnection) super.createConnection(url);
+                    try {
+                        httpsURLConnection.setSSLSocketFactory(getSSLSocketFactory());
+                        httpsURLConnection.setHostnameVerifier(getHostnameVerifier());
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    return httpsURLConnection;
+                }
+            };
+
+            mRequestQueue = Volley.newRequestQueue(getApplicationContext(), hurlStack);
         }
 
         return mRequestQueue;
@@ -44,5 +83,79 @@ public class AppController extends Application {
         if (mRequestQueue != null) {
             mRequestQueue.cancelAll(tag);
         }
+    }
+
+    // Let's assume your server app is hosting inside a server machine
+    // which has a server certificate in which "Issued to" is "localhost",for example.
+    // Then, inside verify method you can verify "localhost".
+    // If not, you can temporarily return true
+    private HostnameVerifier getHostnameVerifier() {
+        return new HostnameVerifier() {
+            @Override
+            public boolean verify(String hostname, SSLSession session) {
+                //return true; // verify always returns true, which could cause insecure network traffic due to trusting TLS/SSL server certificates for wrong hostnames
+                HostnameVerifier hv = HttpsURLConnection.getDefaultHostnameVerifier();
+                return hv.verify("skillsup.ca", session);
+            }
+        };
+    }
+
+    private TrustManager[] getWrappedTrustManagers(TrustManager[] trustManagers) {
+        final X509TrustManager originalTrustManager = (X509TrustManager) trustManagers[0];
+        return new TrustManager[]{
+                new X509TrustManager() {
+                    public X509Certificate[] getAcceptedIssuers() {
+                        return originalTrustManager.getAcceptedIssuers();
+                    }
+
+                    public void checkClientTrusted(X509Certificate[] certs, String authType) {
+                        try {
+                            if (certs != null && certs.length > 0){
+                                certs[0].checkValidity();
+                            } else {
+                                originalTrustManager.checkClientTrusted(certs, authType);
+                            }
+                        } catch (CertificateException e) {
+                            Log.w("checkClientTrusted", e.toString());
+                        }
+                    }
+
+                    public void checkServerTrusted(X509Certificate[] certs, String authType) {
+                        try {
+                            if (certs != null && certs.length > 0){
+                                certs[0].checkValidity();
+                            } else {
+                                originalTrustManager.checkServerTrusted(certs, authType);
+                            }
+                        } catch (CertificateException e) {
+                            Log.w("checkServerTrusted", e.toString());
+                        }
+                    }
+                }
+        };
+    }
+
+    private SSLSocketFactory getSSLSocketFactory()
+            throws CertificateException, KeyStoreException, IOException, NoSuchAlgorithmException, KeyManagementException {
+        CertificateFactory cf = CertificateFactory.getInstance("X.509");
+        InputStream caInput = getAssets().open("skillsup.ca.crt"); // this cert file stored in app/src/main/assets folder path
+
+        Certificate ca = cf.generateCertificate(caInput);
+        caInput.close();
+
+        KeyStore keyStore = KeyStore.getInstance("BKS");
+        keyStore.load(null, null);
+        keyStore.setCertificateEntry("ca", ca);
+
+        String tmfAlgorithm = TrustManagerFactory.getDefaultAlgorithm();
+        TrustManagerFactory tmf = TrustManagerFactory.getInstance(tmfAlgorithm);
+        tmf.init(keyStore);
+
+        TrustManager[] wrappedTrustManagers = getWrappedTrustManagers(tmf.getTrustManagers());
+
+        SSLContext sslContext = SSLContext.getInstance("TLS");
+        sslContext.init(null, wrappedTrustManagers, null);
+
+        return sslContext.getSocketFactory();
     }
 }
