9847a72b77ebb0f38c2a5c4931887531cbe861f9
Dang Quan <dangquan@yahoo.com>
2016-11-25 14:53:59 +1100

    Add script creating db

--- app/src/main/java/com/hue/signupandsignin/app/AppConfig.java
+++ app/src/main/java/com/hue/signupandsignin/app/AppConfig.java
@@ -6,10 +6,12 @@ package com.hue.signupandsignin.app;
 
 public class AppConfig {
     // Server user login url
-    //public static String URL_LOGIN = "http://10.2.4.88:38080/android_login_api/login.php";
-    public static String URL_LOGIN = "https://192.168.1.101:38080/android_login_api/login.php";
+    //public static String URL_LOGIN = "https://10.2.4.88:38080/android_login_api/login.php";
+    public static String URL_LOGIN = "https://10.2.3.36:38080/android_login_api/login.php";
+    //public static String URL_LOGIN = "https://192.168.1.107:38080/android_login_api/login.php";
 
     // Server user register url
-    //public static String URL_REGISTER = "http://10.2.4.88:38080/android_login_api/register.php";
-    public static String URL_REGISTER = "https://192.168.1.101:38080/android_login_api/register.php";
+    //public static String URL_REGISTER = "https://10.2.4.88:38080/android_login_api/register.php";
+    public static String URL_REGISTER = "https://10.2.3.36:38080/android_login_api/register.php";
+    //public static String URL_REGISTER = "https://192.168.1.107:38080/android_login_api/register.php";
 }
